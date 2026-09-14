1435e89a08824d19cf5d0bddce3eb98cc5ced039
Kartik <71930656+kartinul@users.noreply.github.com>
2026-09-15 00:36:17 +0530

    test

--- submissions.sh
+++ submissions.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 curl -s https://api.github.com/repos/kartinul/GitGud/contents/submissions | jq -r '.[].name' | while read -r n; do
 	m=$(curl -s https://raw.githubusercontent.com/kartinul/GitGud/main/submissions/$n/meme_name.txt | tr -d '\r\n')
 	c1=$(curl -s https://raw.githubusercontent.com/kartinul/GitGud/main/submissions/$n/caption1.txt | tr -d '\r\n' | tr ' ' '_')
